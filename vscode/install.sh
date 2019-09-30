ba410dafee6914db9762489020419fb8b5ca5842
Sergey Sudakovich <sergey@sudakovich.com>
2019-09-30 08:51:13 -0700

    Update vscode

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,3 @@
-set -x
 __basedir="$HOME/Library/Application Support/Code/User"
 mkdir -p "$__basedir"
 ln -fs "$PWD/settings.json" "$__basedir/settings.json"
@@ -6,4 +5,3 @@ ln -fs "$PWD/settings.json" "$__basedir/settings.json"
 for extension in $(cat "$PWD/extensions.txt"); do
     code --install-extension "$extension"
 done
-set +x
