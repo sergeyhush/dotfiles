ee5867d08c21bca248c318d2d5850799aa669153
Sergey Sudakovich <sergey@sudakovich.com>
2019-09-29 21:12:40 -0700

    Add VSCode settings

---
+++ vscode/install.sh
@@ -0,0 +1,7 @@
+__basedir="$HOME/Library/Application\ Support/Code/User"
+mkdir -p "$__basedir"
+ln -fs "$PWD/settings.json" "$__basedir/settings.json"
+
+for extension in $(cat "$PWD/extensions.txt"); do
+    code --install-extension "$extension"
+done
