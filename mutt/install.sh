e892617d1593754cba06de3f1743279415c58968
Sergey Sudakovich <sergey@sudakovich.com>
2020-02-28 19:22:39 -0800

    Remove mutt

--- mutt/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "> Install Mutt"
-MUTTDIR="${HOME}/.mutt"
-COLORSDIR="${MUTTDIR}/colors"
-if [ ! -d "${COLORSDIR}" ]; then
-    git clone https://github.com/altercation/mutt-colors-solarized "${COLORSDIR}"
-fi
