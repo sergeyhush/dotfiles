ae4b6dc6dbc914f1d8febb8755b0f543391e0638
Sergey Sudakovich <sergey@sudakovich.com>
2020-04-19 12:05:36 -0700

    Remove zsh/tmux.zsh

--- zsh/tmux.zsh
+++
@@ -1,9 +0,0 @@
-DEFAULT_SESSION='dev'
-
-# Only run if tmux is actually installed
-if which tmux &> /dev/null
-then
-    if [[ -z "$TMUX" && -z "$INSIDE_EMACS" && -z "$EMACS" && -z "$VIM" ]]; then
-        tmux attach-session -d -t $DEFAULT_SESSION || tmux new-session -s $DEFAULT_SESSION
-    fi
-fi
