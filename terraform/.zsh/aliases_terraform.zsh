92cf460a2a18c99930c4e8ed4e49203531b8ce19
Sergey Sudakovich <sergey@sudakovich.com>
2021-02-04 15:30:34 -0800

    update terraform aliases

--- terraform/.zsh/aliases_terraform.zsh
+++ terraform/.zsh/aliases_terraform.zsh
@@ -1,2 +1,12 @@
 alias tf=terraform
-alias tfplan="terraform plan -out=tfplan"
+alias tfv='terraform validate'
+alias tfi='terraform init'
+alias tfp='terraform plan'
+
+# TerraForm MOdule Explained
+function tfmoe {
+  echo -e "\nOutputs:"
+  grep -r "output \".*\"" $1 |awk '{print "\t",$2}' |tr -d '"'
+  echo -e "\nVariables:"
+  grep -r "variable \".*\"" $1 |awk '{print "\t",$2}' |tr -d '"'
+}
