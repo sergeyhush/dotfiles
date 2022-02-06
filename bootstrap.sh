2e8b365b4303f8af1ba2ce05d1d6a0e2c8a15323
Sergey Sudakovich <sergey@sudakovich.com>
2022-02-05 17:59:03 -0800

    update: use global Brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ if ! command -v brew &>/dev/null 2>&1; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 brew analytics off
-brew bundle
+brew bundle --global
 brew cleanup || true
 
 # Chezmoi
