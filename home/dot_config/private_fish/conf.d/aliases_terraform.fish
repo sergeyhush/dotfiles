116b6c20823cc994456062e56271e6e0d2319eb6
sergeyhush <sergey@sudakovich.com>
2024-08-18 18:29:25 -0700

    Remove home/.config/fish/conf.d/aliases_git.fish Remove home/.config/fish/conf.d/aliases_nvim.fish Remove home/.config/fish/conf.d/aliases_terraform.fish

--- home/dot_config/private_fish/conf.d/aliases_terraform.fish
+++
@@ -1,7 +0,0 @@
-alias tf="terraform"
-alias tfa="terraform apply"
-alias tff="terraform fmt"
-alias tfi="terraform init"
-alias tfo='terraform output'
-alias tfp='terraform plan'
-alias tfv='terraform validate'
