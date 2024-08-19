ebe3d200f19cc70404c9d7457ebd42349732ba9e
sergeyhush <sergey@sudakovich.com>
2024-08-18 18:28:03 -0700

    Update home/.config/nvim/lazy-lock.json Update home/.config/nvim/lazyvim.json Update home/.config/nvim/lua/config/lazy.lua Add home/.config/nvim/lua/ssergey/config/autocmds.lua Add home/.config/nvim/lua/ssergey/config/lazy.lua Add home/.config/nvim/lua/ssergey/config/mappings.lua Add home/.config/nvim/lua/ssergey/config/options.lua Add home/.config/nvim/lua/ssergey/plugins/better-escape.lua Add home/.config/nvim/lua/ssergey/plugins/comment.lua Add home/.config/nvim/lua/ssergey/plugins/conform.lua Add home/.config/nvim/lua/ssergey/plugins/debug.lua Add home/.config/nvim/lua/ssergey/plugins/gitsigns.lua Add home/.config/nvim/lua/ssergey/plugins/mini.lua Add home/.config/nvim/lua/ssergey/plugins/nvim-cmp.lua Add home/.config/nvim/lua/ssergey/plugins/nvim-lint.lua Add home/.config/nvim/lua/ssergey/plugins/nvim-lspconfig.lua Add home/.config/nvim/lua/ssergey/plugins/nvim-treesitter.lua Add home/.config/nvim/lua/ssergey/plugins/oil.lua Add home/.config/nvim/lua/ssergey/plugins/telescope.lua Add home/.config/nvim/lua/ssergey/plugins/todo-comments.lua Add home/.config/nvim/lua/ssergey/plugins/ui.lua Add home/.config/nvim/lua/ssergey/plugins/vim-sleuth.lua Add home/.config/nvim/lua/ssergey/plugins/which-key.lua

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 2592000 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
---
+++ home/dot_config/nvim/lua/ssergey/config/lazy.lua
@@ -0,0 +1,13 @@
+-- [[ Configure and install plugins ]]
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("ssergey.plugins")
+
+-- vim: ts=2 sts=2 sw=2 et
