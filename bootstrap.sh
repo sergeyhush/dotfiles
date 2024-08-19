62d80e2fa8b644d17bc3dddcf648f9c0a14ddfe9
sergeyhush <sergey@sudakovich.com>
2024-08-18 19:18:37 -0700

    fix: move setup to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,11 @@ set -x
 
 if test "${CI:-}"; then
     set -x # For better debugging
+    mkdir -p $HOME/.config/chezmoi/ $HOME/.local/share
+    touch $HOME/.config/chezmoi/chezmoi.toml
+    echo '[data]' >>$HOME/.config/chezmoi/chezmoi.toml
+    echo 'email = "john.doe@example.com"' >>$HOME/.config/chezmoi/chezmoi.toml
+    echo 'fullname = "John Doe"' >>$HOME/.config/chezmoi/chezmoi.toml
     # Source: https://discuss.circleci.com/t/brew-link-step-failing-on-python-dependency/33925/8?u=ashishb
     export HOMEBREW_NO_AUTO_UPDATE=1
     export HOMEBREW_NO_INSTALL_CLEANUP=1
