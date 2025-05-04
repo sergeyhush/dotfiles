beaaa979aff5e4cd7bfb7e2fa935628c80c09c33
Sergey Sudakovich <64040+sergeyhush@users.noreply.github.com>
2025-05-03 21:56:26 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
-set -x
+set -exuo pipefail
 
-if test "${CI:-}"; then
-    set -x # For better debugging
-    mkdir -p $HOME/.config/chezmoi/ $HOME/.local/share
-    cat << EOL > $HOME/.config/chezmoi/chezmoi.toml.tmpl
-    [data]
-        fullname = "John Doe"
-        email = "john.doe@example.com"
-        personal = true
-    [git]
-        autoCommit = false
-        autoPush = false
-    EOL
-
-    # Source: https://discuss.circleci.com/t/brew-link-step-failing-on-python-dependency/33925/8?u=ashishb
-    export HOMEBREW_NO_AUTO_UPDATE=1
-    export HOMEBREW_NO_INSTALL_CLEANUP=1
-fi
+# Source: https://discuss.circleci.com/t/brew-link-step-failing-on-python-dependency/33925/8?u=ashishb
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_CLEANUP=1
 
 # Chezmoi
 if ! command -v chezmoi &>/dev/null; then
@@ -30,8 +15,8 @@ chezmoi init sergeyhush --apply --force --branch chezmoi
 # Homebrew
 if ! command -v brew &>/dev/null 2>&1; then
     echo "Installing homebrew..."
-    ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 brew analytics off
 brew bundle --global
-brew cleanup || true
+brew cleanup
