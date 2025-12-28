c23ceea4f6d6f6c226772e041bcfdf982e7a305f
Sergey Sudakovich <sergey@sudakovich.com>
2025-12-27 22:20:21 -0800

    Add .config/fish/conf.d/_tide_init.fish Add .config/fish/conf.d/abbr_tips.fish Add .config/fish/conf.d/aliases_git.fish Add .config/fish/conf.d/aliases_nvim.fish Add .config/fish/conf.d/aliases_terraform.fish Add .config/fish/conf.d/homebrew.fish Add .config/fish/conf.d/kubectl_aliases.fish Add .config/fish/functions/__abbr_tips_bind_newline.fish Add .config/fish/functions/__abbr_tips_bind_space.fish Add .config/fish/functions/__abbr_tips_clean.fish Add .config/fish/functions/__abbr_tips_init.fish Add .config/fish/functions/fish_mode_prompt.fish Add .config/fish/functions/fish_prompt.fish Add .config/fish/functions/tide.fish Add .config/fish/functions/tide/configure/choices/all/finish.fish Add .config/fish/functions/tide/configure/choices/all/icons.fish Add .config/fish/functions/tide/configure/choices/all/prompt_colors.fish Add .config/fish/functions/tide/configure/choices/all/prompt_connection.fish Add .config/fish/functions/tide/configure/choices/all/prompt_connection_andor_frame_color.fish Add .config/fish/functions/tide/configure/choices/all/prompt_spacing.fish Add .config/fish/functions/tide/configure/choices/all/show_time.fish Add .config/fish/functions/tide/configure/choices/all/style.fish Add .config/fish/functions/tide/configure/choices/classic/classic_prompt_color.fish Add .config/fish/functions/tide/configure/choices/classic/classic_prompt_separators.fish Add .config/fish/functions/tide/configure/choices/lean/lean_prompt_height.fish Add .config/fish/functions/tide/configure/choices/powerline/powerline_prompt_frame.fish Add .config/fish/functions/tide/configure/choices/powerline/powerline_prompt_heads.fish Add .config/fish/functions/tide/configure/choices/powerline/powerline_prompt_height.fish Add .config/fish/functions/tide/configure/choices/powerline/powerline_prompt_tails.fish Add .config/fish/functions/tide/configure/choices/rainbow/rainbow_prompt_separators.fish Add .config/fish/functions/tide/configure/configs/classic.fish Add .config/fish/functions/tide/configure/configs/classic_16color.fish Add .config/fish/functions/tide/configure/configs/lean.fish Add .config/fish/functions/tide/configure/configs/lean_16color.fish Add .config/fish/functions/tide/configure/configs/rainbow.fish Add .config/fish/functions/tide/configure/configs/rainbow_16color.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_cache_variables.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_item_character.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_item_cmd_duration.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_item_git.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_item_newline.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_item_os.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_item_time.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_print_item.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_prompt.fish Add .config/fish/functions/tide/configure/functions/_fake_tide_pwd.fish Add .config/fish/themes/Catppuccin Mocha.theme

---
+++ home/dot_config/private_fish/conf.d/aliases_terraform.fish
@@ -0,0 +1,7 @@
+alias tf="terraform"
+alias tfa="terraform apply"
+alias tff="terraform fmt"
+alias tfi="terraform init"
+alias tfo='terraform output'
+alias tfp='terraform plan'
+alias tfv='terraform validate'
---
+++ home/dot_config/private_fish/conf.d/kubectl_aliases.fish
@@ -0,0 +1,5 @@
+abbr --add k kubectl
+abbr --add kd "kubectl describe"
+abbr --add kdpo "kubectl describe pods"
+abbr --add kg "kubectl get"
+abbr --add kgpo "kubectl get pods"
