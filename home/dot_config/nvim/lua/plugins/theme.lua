ec364cfa226de0ee860c28f3e6a2c72493973119
Sergey Sudakovich <sergey@sudakovich.com>
2025-12-27 21:10:23 -0800

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/theme.lua

--- home/dot_config/nvim/lua/plugins/theme.lua
+++ home/dot_config/nvim/lua/plugins/theme.lua
@@ -1,18 +1,13 @@
 return {
   -- add tokyonight
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-      style = "night",
-    },
-  },
-
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   opts = {
+  --     style = "night",
+  --   },
+  -- },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 }
