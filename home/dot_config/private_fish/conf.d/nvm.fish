e9e73df47df7d180263f1b127300b3dc37ff819e
sergeyhush <sergey@sudakovich.com>
2025-12-31 12:49:37 -0800

    Remove .config/fish/conf.d/nvm.fish Remove .config/fish/functions/__abbr_tips_bind_newline.fish Remove .config/fish/functions/__abbr_tips_bind_space.fish Remove .config/fish/functions/__abbr_tips_clean.fish Remove .config/fish/functions/__abbr_tips_init.fish

--- home/dot_config/private_fish/conf.d/nvm.fish
+++
@@ -1,3 +0,0 @@
-## nvm.fish disabled in favor of `mise`
-## Keeping this file as a stub so it can be re-enabled later if needed.
-## To re-enable, restore the original contents from version control.
