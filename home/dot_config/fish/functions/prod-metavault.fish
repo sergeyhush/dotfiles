8293e7361f35f15454512d5e0f60e70ca926ce9a
Sergey Sudakovich <sergey@sudakovich.com>
2025-12-31 17:05:50 -0800

    Remove .config/fish/functions/prod-metavault.fish

--- home/dot_config/fish/functions/prod-metavault.fish
+++
@@ -1,9 +0,0 @@
-function prod-metavault
-    set -gx VAULT_SERVER_URL https://prod-metavault.int.snowflakecomputing.com:8200
-    set -gx VAULT_ADDR $VAULT_SERVER_URL
-
-    vault login -method=okta username=$USER password=(op item list --tags sfcli-snowflake-okta --format json | op item get --fields label=password --reveal -)
-
-    set -gx VAULT_SERVER_TOKEN (cat ~/.vault-token)
-    set -gx VAULT_TOKEN $VAULT_SERVER_TOKEN
-end
